1f02d9292b41b222f9eb66027178d9f34bb03ed9
oleg-programmistjava <programmistjava@yandex.ru>
2024-06-23 15:20:35 +0300

    Поправил ряд мелких неточностей. И довел приложение до первого логическоего завершения.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,27 +8,34 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
 
         char delivery;
-        double deliveryPrice = 25;
+        double deliveryPrice = 25.00;
         double cdPrice = 5.25;
+        double totalPrice;
 
         System.out.println("Добро пожаловать в магазин для геймеров");
         System.out.println("---------------------------------------");
         System.out.println("Сегодня у нас распродажа игровых дисков:");
         System.out.println("На все диски скидка 10%");
 
-        System.out.println("Рассчитать стоимость доставки? (Да/Нет)");
+        System.out.print("Рассчитать стоимость доставки? (Да/Нет) ");
         delivery = keyboard.findWithinHorizon(".",0).charAt(0);
 
         if (delivery == 'Д' || delivery == 'д') {
 
+            System.out.println("---");
             System.out.println("Доставка составит: " + deliveryPrice);
             System.out.println("Цена выбранного Вами диска: " + cdPrice + " долларов.");
+            System.out.print("Итоговая стоимость диска включая доставку составит: ");
+            totalPrice = cdPrice + deliveryPrice;
+            System.out.println(totalPrice + " рублей.");
 
         }
 
-        if (delivery != 'Д' || delivery != 'д' ) {
+        if (delivery != 'Д' && delivery != 'д' ) {
 
-            System.out.println("Вы можете забрать Ваш диск в любое удобное для Вас время.");
+            System.out.println("---");
+            System.out.println("Вы выбрали САМОВЫВОЗ");
+            System.out.println("И поэтому - Вы можете забрать Ваш диск в любое удобное для Вас время.");
 
         }
 
