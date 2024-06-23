32d8906188700df9c72d4762ff678dc8af44cc35
oleg-programmistjava <programmistjava@yandex.ru>
2024-06-23 14:38:57 +0300

    Создали шапку приложения

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Добро пожаловать в магазин для геймеров");
+        System.out.println("---------------------------------------");
+        System.out.println("Сегодня у нас распродажа игровых дисков:");
+        System.out.println("На все диски скидка 10%");
+
+    }
+
+}
